6ddea24c00ef41e75a0c3f28012c00c86414f0e3
Vatsalay <43850038+kvatsalay@users.noreply.github.com>
2019-04-29 23:31:03 +0530

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, flash, url_for, redirect
+from forms import ContactForm
+from flask_mail import Message, Mail 
+
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisismysecret'
+
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'vatsalayvk1434@gmail.com'
+app.config['MAIL_PASSWORD'] = 'pxqv ilao wlyo coee'
+
+mail = Mail(app)
+
+
+
+
+# mail.init_app(app)
+
+@app.route('/index')
+def index():
+	return render_template('index3.html', title='Flask Index', success=True)
+
+@app.route('/form', methods=['GET', 'POST'])
+def contactForm():
+	form = ContactForm()
+	if request.method == 'GET':
+		return render_template('contact.html', form=form)
+	elif request.method == 'POST':
+		if form.validate() == False:
+			flash('All fields are required !')
+			return render_template('contact.html', form=form)
+		else:
+			msg = Message(form.subject.data, sender='[SENDER EMAIL]', recipients=['vatsalayvk1434@gmail.com'])
+			msg.body = """
+			from: %s &lt;%s&gt
+			%s
+			"""% (form.name.data, form.email.data, form.message.data)
+			mail.send(msg)
+			return redirect(url_for('index'))
+		return '<h1>Form submitted!</h1>'
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
